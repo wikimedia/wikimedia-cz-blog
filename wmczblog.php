c7aa6c2a79bc160656612b709f955cfd329b6c8e
Martin Urbanec <martin.urbanec@wikimedia.cz>
2020-11-12 23:38:50 +0100

    Revert "Register wdqs as a wordpress embed handler via wp_embed_register_handler"
    This reverts commit d3d38a306ee3eada7475138e46ba3e5ac9996ffb.
    
    Reason: Split to github.com/urbanecm/wordpress-wdqs-embed
    Change-Id: I93aa4713e4ccf8ed885e27cf437bdc9d8f2a5db2

--- wmczblog.php
+++ wmczblog.php
@@ -1,13 +1,14 @@
 <?php 
-/**
- * Plugin Name: WMCZ Blog - vlastní kód a CSS
- * Plugin URI: http://wikimedia.cz
- * Description: Plugin aplikující vlastní zmeny a prizpusobení
- * Version: 1.0
- * Author: Dominik Matus, Martin Urbanec, Wikimedia Czech Republic
- * Author URI: http://dominikmatus.cz/
- * License: GPLv2
-**/
+/*
+Plugin Name: WMCZ Blog - vlastní kód a CSS
+Plugin URI: http://wikimedia.cz
+Description: Plugin aplikující vlastní zmeny a prizpusobení
+Version: 1.0
+Author: Dominik Matus
+Author URI: http://dominikmatus.cz/
+License: GPLv2
+*/ 
+  
 
 function add_meta_tags() {      
 
@@ -15,22 +16,6 @@ function add_meta_tags() {
 }
 add_action( 'wp_head', 'add_meta_tags' , 2 );
 
-function wmcz_handle_wdqs_embed( $matches, $attr, $url, $rawattr ) {
-        global $content_width;
-        return sprintf(
-                '<iframe style="width: 80vw; height: 50vh; border: none;" src="https://query.wikidata.org/embed.html#%s" referrerpolicy="origin" sandbox="allow-scripts allow-same-origin allow-popups"></iframe>',
-                esc_attr($matches[1])
-        );
-}
 
-add_action(
-        'init', function() {
-                wp_embed_register_handler(
-                        'wdqs',
-                        '@https?://query\.wikidata\.org/[^#]*#(.*)@i',
-                        'wmcz_handle_wdqs_embed'
-                );
-        }
-);
 
 ?>
